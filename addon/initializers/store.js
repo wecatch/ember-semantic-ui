159a22bf811fb6685d89fecf24859381d209ce16
zhyq0826 <zhyq0826@gmail.com>
2015-09-16 12:53:14 +0800

    rm orm and form modify package.json keywords

--- addon/initializers/store.js
+++
@@ -1,9 +0,0 @@
-export function initialize( container, application ) {
-  application.inject('route', 'store', 'service:store');
-  application.inject('controller', 'store', 'service:store');
-}
-
-export default {
-  name: 'store',
-  initialize: initialize
-};
