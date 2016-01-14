9a301601832f83dc7be3e594e5078ebb3467b27a
zhyq0826 <zhyq0826@gmail.com>
2016-01-14 11:14:48 +0800

    fix #11

--- addon/components/ui-input-tags.js
+++ addon/components/ui-input-tags.js
@@ -19,21 +19,8 @@ export default Ember.Component.extend({
      * @property {Ember.String} value
      */
     value: null,
-
-    /**
-     * @function initialize
-     * @observes "didInsertElement" event
-     * @returns  {void}
-     */
-    initialize: function(argument) {
-        this.renderDropDown();
-    }.on('didInsertElement'),
     renderDropDown: function() {
         let that = this;
-        let selectDom = this.assembleDom();
-        this.$().empty();
-        this.$().append(selectDom);
-
         this.$('select').dropdown({
             allowAdditions: true,
             onAdd: function(addedValue, addedText, $addedChoice) {
@@ -47,23 +34,12 @@ export default Ember.Component.extend({
                 that.set('value', value.toArray());
             }
         });
-    }.observes('value.[]'),
-    assembleDom: function() {
-
-        // init select
-        let selectDom = '<select multiple="" class="' + this.get('_uiClass') + ' ' + this.get('theme') + ' ' + this.get('_componentClass') + '">';
-        let value = this.get('value');
-
-        for (var i = 0; i < value.length; i++) {
-            let item = String(value[i]);
-            let option = '';
-            option = '<option value="' + item + '" selected="" >' + item + '</option>';
-            selectDom += option;
-        };
-
-        selectDom += '</select>';
-        return selectDom;
     },
+    valueChange: Ember.observer('value', function() {
+        Ember.run.scheduleOnce('afterRender', this, function() {
+            this.renderDropDown();
+        });
+    }),
     init: function() {
         this._super(...arguments);
         if (!this.get('value')) {
