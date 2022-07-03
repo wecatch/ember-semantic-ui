9d594c0111abe25fa822b50e5188d39be441a58e
zhaoyongqiang <zhyq0826@gmail.com>
2022-07-03 22:34:52 +0800

    fix ui input tags

--- addon/components/ui-input-tags.js
+++ addon/components/ui-input-tags.js
@@ -1,10 +1,7 @@
-import Ember from 'ember';
-import layout from './ui-input-tags';
-import Component from '@ember/component';
-import { guidFor } from '@ember/object/internals';
 import { A } from '@ember/array';
-import { computed } from '@ember/object';
-import { isArray } from '@ember/array';
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import $ from 'jquery';
 
 /**
@@ -15,85 +12,50 @@ ui-input-tags component
 @class UiInputTags
 @constructor
 */
-export default Component.extend({
-  layout: layout,
-  tagName: 'div',
+export default class UiInputTagsComponent extends Component {
 
-  /**
-   * Class name to apply to the button
-   *
-   * @property {String} theme
-   */
-  theme: 'fluid',
-  /**
-   * Class name to apply to the button
-   *
-   * @property {String} class
-   */
-  class: '',
-  classNameBindings: ['_uiClass', 'class', 'theme', '_componentClass'],
-  _uiClass: 'ui',
-  _componentClass: 'multiple search selection dropdown',
+  @tracked value = A();
 
-  renderDropDown: function () {
-    let that = this;
-    this.$().dropdown({
+  @action
+  register(element) {
+    $(element).dropdown({
       allowAdditions: true,
-      onAdd: function (addedValue) {
-        that._addValue(addedValue);
+      onAdd: (addedValue) => {
+        this._addValue(addedValue);
       },
-      onRemove: function (removedValue) {
-        that._removeValue(removedValue);
+      onRemove: (removedValue) => {
+        this._removeValue(removedValue);
       },
       onLabelCreate: function (label) {
-        that.$('input.search').val('');
-        that.$('.addition.item b').html('');
+        $(element).find('input.search').val('');
+        $(element).find('.addition.item b').html('');
         return $(label);
       },
     });
-  },
-  didInsertElement() {
-    this._super(...arguments);
-    this.renderDropDown();
-  },
+  }
   _addValue(value) {
-    try {
-      this.value.addObject(value);
-    } catch (e) {
-      let id = guidFor(this);
-      Ember.Logger.warn(
-        `component:ui-input-tags ${id} value is not Ember.A type`
-      );
-      Ember.Logger.error(e);
+    this.value.addObject(value)
+    if (typeof this.args.onChange === 'function') {
+      this.args.onChange(this.value);
     }
-    if (typeof this.attrs.update === 'function') {
-      this.attrs.update(this.value);
-    }
-  },
+  }
+
   _removeValue(value) {
     this.value.removeObject(value);
-    if (typeof this.attrs.update === 'function') {
-      this.attrs.update(this.value);
+    if (typeof this.args.onChange === 'function') {
+      this.args.onChange(this.value);
     }
-  },
-  init() {
-    //if value do not be passed to component
-    this._super(...arguments);
-    if (this.value === undefined) {
-      this.set('value', A());
-    }
+  }
 
-    for (var i = this.value.length - 1; i >= 0; i--) {
-      this.value.set(String(i), String(this.value[i]));
+  constructor() {
+    super(...arguments);
+    for (var i = this.args.value.length - 1; i >= 0; i--) {
+      this.value.set(String(i), String(this.args.value[i]));
     }
-  },
-  hiddenValue: computed('value', {
-    get() {
-      if (isArray(this.value)) {
-        return this.value.join(',');
-      } else {
-        return '';
-      }
-    },
-  }),
-});
+  }
+
+  get hiddenValue() {
+    return this.value.join(',');
+  }
+}
+
