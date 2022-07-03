b8d1047dd75f2da871d693326f3f976d6b0ce147
zhaoyongqiang <zhyq0826@gmail.com>
2022-07-03 23:18:02 +0800

    fix ui checkbox

--- addon/components/ui-checkbox.js
+++ addon/components/ui-checkbox.js
@@ -1,6 +1,7 @@
-import UiCheckboxBase from '../mixins/ui-checkbox-base';
-import layout from './ui-checkbox';
-import Component from '@ember/component';
+
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
+import $ from 'jquery';
 
 /**
 ui-checkbox component see {{#crossLink "mixins.UiCheckboxBase"}}{{/crossLink}}
@@ -10,6 +11,50 @@ ui-checkbox component see {{#crossLink "mixins.UiCheckboxBase"}}{{/crossLink}}
 @class UiCheckbox
 @constructor
 */
-export default Component.extend(UiCheckboxBase, {
-  layout: layout,
-});
+export default class UiCheckboxComponent extends Component {
+
+  constructor(){
+    super(...arguments);
+    if(this.checked && this.args.onChange ){
+      this.args.onChange(this.value);
+    }
+  }
+
+  get name() {
+    return this.args.name ?? "";
+  }
+
+  get value() {
+    return this.args.value ?? "";
+  }
+
+  get checked() {
+    return this.args.checked ?? false;
+  }
+
+  @action
+  register(element) {
+    $(element).checkbox();
+    let input = $(element).find('input');
+
+    //set checbox stated
+    input.prop('checked', this.checked);
+    //bind input change event
+    $(element).on("click", () => {
+      let isChecked = input.is(':checked');
+      this._updateValue(isChecked);
+    });
+  }
+
+  _updateValue(checked) {
+    if (typeof this.args.onChange === 'function') {
+      if (checked) {
+        this.args.onChange(this.value);
+        this.args.onCheck(true);
+      } else {
+        this.args.onChange('');
+        this.args.onCheck(false);
+      }
+    }
+  }
+}
--- tests/dummy/app/controllers/ui-checkbox.js
+++ tests/dummy/app/controllers/ui-checkbox.js
@@ -1,10 +1,17 @@
 import Controller from '@ember/controller';
+import {action} from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default Controller.extend({
-  value: null,
-  actions: {
-    checkedCheck(checked, value) {
-      console.log(`${checked} ${value}`);
-    },
-  },
-});
+export default class UiCheckboxController extends Controller {
+  @tracked value = null;
+  @tracked checked = false;
+
+  @action
+  onCheck(checked) {
+    this.checked = checked;
+  }
+  @action
+  onChange(value) {
+    this.value = value;
+  }
+}
