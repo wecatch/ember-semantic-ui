760f32a6c6ab12a921890ac09a4588f2993bfe25
zhaoyongqiang <zhyq0826@gmail.com>
2022-07-10 22:19:15 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,9 @@ cp -r dist docs $target/.
 mv index.html $target/.
 cd $target
 mv dist demo
+git branch -D gh-pages
+git checkout -b gh-pages
+git rm .gitignore
+git add docs demo index.html
+git commit -m"deploy"
+git push origin gh-pages --force
