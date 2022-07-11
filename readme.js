a521250d1ce2b94f7e6e6a3012549d842902e403
zhaoyongqiang <zhyq0826@gmail.com>
2022-07-11 23:54:14 +0800

    fix readme

--- readme.js
+++ readme.js
@@ -12,6 +12,7 @@ const htmlBody = `
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./demo/assets/vendor.css">
+  <link rel="stylesheet" href="./demo/assets/semantic.min.css">
   <link rel="stylesheet" href="./assets/app.css">
   <title>Ember Semantic Ui</title>
   <style>
